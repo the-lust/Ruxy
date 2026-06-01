e1d02c0703b99e3612fffb5102314fbba2e77087
Ahum Maitra <theahummaitra@gmail.com>
2026-06-01 09:26:26 +0530

    feat: Add jokes command to get jokes

--- Src/commands/fun.py
+++ Src/commands/fun.py
@@ -3,6 +3,10 @@
 
 from discord import Interaction
 from datetime import timedelta
+import pyjokes
+import blacklist
+from utility import is_jailed
+
 
 def setup(tree, client):
     @tree.command(
@@ -34,4 +38,25 @@ def setup(tree, client):
                 f"Timed out for {duration} minutes."
             )
         except Exception as e:
-            await interaction.response.send_message(f"Error: {e}", ephemeral=True)
+            await interaction.response.send_message(
+                "Failed to apply timeout. Please try again!  **If not working for a long time, contact support team!**", ephemeral=True
+            )
+
+    @tree.command(name="joke", description="Get a random joke")
+    async def send_joke(interaction: Interaction):
+        if blacklist.is_blacklisted(interaction.user.id):
+            await interaction.response.send_message("You are blacklisted!")
+            return
+        elif is_jailed(interaction):
+            await interaction.response.send_message("You are in jail!")
+            return
+
+        try:
+            joke = pyjokes.get_joke()
+            await interaction.response.send_message(
+                f"**Here's a joke for you:** \n{joke}"
+            )
+        except Exception as e:
+            await interaction.response.send_message(
+                "Failed to fetch a joke. Try again later! **If not working for a long time, contact support team!**", ephemeral=True
+            )
